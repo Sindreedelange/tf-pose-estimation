da89c343f5767ef0d4eb637c344ce74bd293b672
sindreedelange <146169@stud.hvl.no>
2019-03-06 14:50:57 +0100

    Remove unnecessary error

--- tf_pose/networks.py
+++ tf_pose/networks.py
@@ -95,6 +95,6 @@ def get_graph_path(model_name):
 
 def model_wh(resolution_str):
     width, height = map(int, resolution_str.split('x'))
-    if width % 16 != 0 or height % 16 != 0:
-        raise Exception('Width and height should be multiples of 16. w=%d, h=%d' % (width, height))
+    #if width % 16 != 0 or height % 16 != 0:
+    #    raise Exception('Width and height should be multiples of 16. w=%d, h=%d' % (width, height))
     return int(width), int(height)
