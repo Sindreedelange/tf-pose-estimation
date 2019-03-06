5cbe8d84bd14f2aa2dcf811ba2461cbf21928ac5
sindreedelange <146169@stud.hvl.no>
2019-03-06 13:56:38 +0100

    Fix video reading

--- run_video.py
+++ run_video.py
@@ -4,6 +4,7 @@ import time
 
 import cv2
 import numpy as np
+import time
 
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
@@ -20,35 +21,46 @@ fps_time = 0
 
 
 if __name__ == '__main__':
+    start = time.time()
     parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
     parser.add_argument('--video', type=str, default='')
-    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
+    parser.add_argument('--resolution', type=str, default='340x256', help='network input resolution. default=432x368')
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
     parser.add_argument('--show-process', type=bool, default=False,
                         help='for debug purpose, if enabled, speed for inference is dropped.')
     parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
-    args = parser.parse_args()
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+    parser.add_argument('--output_json', type=str, default='tmp/', help='writing output json dir')
 
+    args = parser.parse_args()
+    end_parameters = time.time() - start
     logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
     w, h = model_wh(args.resolution)
     e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
-    cap = cv2.VideoCapture(args.video)
-
-    if cap.isOpened() is False:
+    end_load_estimator = time.time() - start
+    vid = cv2.VideoCapture(args.video)
+    success, image = vid.read()
+    if not success:
         print("Error opening video stream or file")
-    while cap.isOpened():
-        ret_val, image = cap.read()
 
-        humans = e.inference(image)
+    frame = 0
+    while success:
+        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
         if not args.showBG:
             image = np.zeros(image.shape)
-        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False, frame=frame, output_json_dir=args.output_json)
+        frame += 1
 
         cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
         if cv2.waitKey(1) == 27:
             break
+        success, image = vid.read()
 
     cv2.destroyAllWindows()
+    logger.debug("Arguments time: {}".format(end_parameters))
+    logger.debug("estimator time: {}".format(end_load_estimator))
 logger.debug('finished+')
+
